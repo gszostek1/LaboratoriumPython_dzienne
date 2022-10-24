95245cc88bd977143b96fcd0fd1bacf67784b2d4
gszostek1 <115081244+gszostek1@users.noreply.github.com>
2022-10-24 15:35:16 +0200

    Delete 4.py

--- Lab1/4.py
+++
@@ -1,7 +0,0 @@
-znak = input('Wprowadź znak: ')
-if znak >= 'a' and znak <= 'z':
-    jaka = 'jest małą'
-elif znak >= 'A' and znak <= 'Z':
-    jaka = 'jest dużą'
-else: jaka = 'znak nie jest literą'
-print(f'Podany znak {jaka} literą')

a99b516483f49693abf6c1e6252273ba53efab44
gszostek1 <115081244+gszostek1@users.noreply.github.com>
2022-10-27 11:47:09 +0200

    Delete Lab3_3 directory

--- Lab3_3/2.py
+++
@@ -1,5 +0,0 @@
-# Napisz program, który wyświetli wartości funkcji 𝑦 = 2𝑥
-# 2 − 5𝑥 − 8, dla 𝑥 ∈ [−4, 4], z krokiem
-# 0.5.
-
-
--- Lab3_3/4.py
+++
@@ -1,7 +0,0 @@
-x=int(input("Podaj liczbę"))
-y=int(input("Podaj Liczbę"))
-if y<x:
-    x,y=y,x
-while y>=x:
-    print(x,end=" ")
-    x = x + 1
